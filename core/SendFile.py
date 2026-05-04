faf717a1a5f0f3f23d2d674879e2bb557b384764
belzEnn <vladimirlytvynenko1@gmail.com>
2026-05-05 01:00:37 +0300

    refactor: optimize file upload with async I/O and concurrent processing

--- core/SendFile.py
+++ core/SendFile.py
@@ -1,4 +1,6 @@
 import os
+import aiofiles  # Нужно доустановить
+import asyncio
 from database.models import FileBase
 #Send file func
 async def Send(client, target, file_path):
@@ -7,24 +9,22 @@ async def Send(client, target, file_path):
         print("File send!")
         print(message.id)
         return message
-        
-        
-
+    
 #Split files to chunks function
 async def Split(file_path: str, chunk_size:int = 1*1024*1024,):       #default chunk size - 1mb
     chunk_list = []     #a list, where is the list of chunks, to merge them back in the future
-    with open(file_path, "rb") as f:
+    async with aiofiles.open(file_path, "rb") as f:
         chunk_num = 0     #func for creating chunk file's name
         while True:
-            chunk = f.read(chunk_size)    #variable with data from file, as big as chunk_size
+            chunk = await f.read(chunk_size)    #variable with data from file, as big as chunk_size
             if not chunk:
                 break
             fileNameOnly = os.path.splitext(file_path)
             chunk_name = f"{fileNameOnly[0]}{chunk_num}.ddd"
-            with open(chunk_name, "wb") as k:
-                k.write(chunk)
+
+            async with aiofiles.open(chunk_name, "wb") as k:
+                await k.write(chunk)
+            
             chunk_list.append(chunk_name)
             chunk_num += 1
     return chunk_list
-
-
